50b9adba3685ebd289c8cc56072d18b596a34ffe
ng-bilel <bilel.feki.92@gmail.com>
2025-05-04 17:28:11 +0100

    add default user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'argon-dashboard-angular';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if (!localStorage.getItem("name")) {
+      localStorage.setItem("name", "emna");
+      localStorage.setItem("email", "emna@gmail.com");
+      localStorage.setItem("password", "123");
+      localStorage.setItem("phoneNumber", "21033558");
+      localStorage.setItem("dateEmbauche", "");
+      localStorage.setItem("cin", "1233");
+      localStorage.setItem("role", "manager");
+      localStorage.setItem("addresse", "tunis");
+    }
+  }
+  title = "argon-dashboard-angular";
 }
